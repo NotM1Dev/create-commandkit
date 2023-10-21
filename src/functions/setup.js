a12121db9e0bdce4f36bd59006c27508b0ef139c
m1-dev <=>
2023-10-21 21:52:50 +0300

    refactor: setup.js

--- src/functions/setup.js
+++ src/functions/setup.js
@@ -4,7 +4,6 @@ const { commands } = require('../config');
 const fs = require('fs-extra');
 const path = require('path');
 
-
 module.exports = async ({ manager, dir, token, type, stdio = 'pipe' }) => {
     await fs.emptyDir(dir);
     ex(commands.init[manager], { cwd: dir, stdio });
@@ -14,8 +13,10 @@ module.exports = async ({ manager, dir, token, type, stdio = 'pipe' }) => {
 
     packageJson.name = packageJson.name.toLowerCase();
     packageJson.type = type == 'esm' ? 'module' : 'commonjs';
+
     packageJson.main = './src/index.js';
     packageJson.version = '0.0.0';
+
     packageJson.scripts = {
         start: 'node ./src/index.js',
         dev: 'nodemon --ext js,json,ts ./src/index.js'
