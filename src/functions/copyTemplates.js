2e0c5ddcf0f19a8bc338b94462f7e8ac944557fb
m1-dev <=>
2023-10-21 21:52:22 +0300

    chore: remove useless import

--- src/functions/copyTemplates.js
+++ src/functions/copyTemplates.js
@@ -1,6 +1,4 @@
 const { templates } = require('../config');
-const { execSync: ex } = require('child_process');
-
 const fs = require('fs-extra');
 
 module.exports = async ({ type, dir, lang }) => {
